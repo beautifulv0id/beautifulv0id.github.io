3b91e2afe486255e111b121891204cc13267c280
Felix Herrmann <felix.rodau@gmail.com>
2020-08-28 14:58:05 +0200

    add documentation

---
+++ group__PkgPointSetProcessing3Ref.js
@@ -0,0 +1,8 @@
+var group__PkgPointSetProcessing3Ref =
+[
+    [ "Named Parameters for Point Set Processing", "group__psp__namedparameters.html", null ],
+    [ "Algorithms", "group__PkgPointSetProcessing3Algorithms.html", "group__PkgPointSetProcessing3Algorithms" ],
+    [ "IO (XYZ/OFF Formats)", "group__PkgPointSetProcessing3IO.html", "group__PkgPointSetProcessing3IO" ],
+    [ "IO (PLY Format)", "group__PkgPointSetProcessing3IOPly.html", "group__PkgPointSetProcessing3IOPly" ],
+    [ "IO (LAS Format)", "group__PkgPointSetProcessing3IOLas.html", "group__PkgPointSetProcessing3IOLas" ]
+];
